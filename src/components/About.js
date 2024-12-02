87e10e7a750f4a76eb2a486eb1b78b681dc1c5c6
Deepak Sharma <dsharma2828@gmail.com>
2024-12-02 23:20:17 +0530

    updated code

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-      
+      <h1>This is About Section</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatem.</p>
     </div>
   )
 }
